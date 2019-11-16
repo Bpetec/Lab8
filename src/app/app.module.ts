265c872607fd8972dc64ac97f08d457ed2a4d61b
bpetec <bpetec@yahoo.com>
2019-11-15 20:52:42 -0800

    finished lab 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { FormsModule } from '@angular/forms';
+// added for ngModel ^
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+    // HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/contact/contact.model.ts
@@ -0,0 +1,35 @@
+interface IContact {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    phone?: string;
+    editing?: boolean;
+}
+export class Contact {
+    public id?: number;
+    public firstName?: string;
+    public lastName?: string;
+    public email?: string;
+    public phone?: string;
+    public editing?: boolean;
+
+    constructor(contact: IContact) {
+        contact.editing = this.setState(contact);
+        Object.assign(this, contact);
+        // this.firstName = contact.firstName; longer way of doing ^
+    }
+    setState(contact: IContact) {
+        if (contact == null || Object.keys(contact).length === 0) {
+            return true;
+        }
+        let editing = false;
+        Object.keys(contact).forEach((key) => {
+            console.log('from setState', contact[key]);
+            if (contact[key] == null) {
+                editing = true;
+            }
+        });
+        return editing;
+    }
+}
